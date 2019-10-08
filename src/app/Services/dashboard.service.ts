94c746a749b90b8496b490bc79f4b70fe50b3572
amareshadak@yahoo.com <Amaresh>
2019-10-08 16:21:06 +0530

    Update latest changes

--- src/app/Services/dashboard.service.ts
+++ src/app/Services/dashboard.service.ts
@@ -10,27 +10,27 @@ constructor( private http: HttpClient ) {  }
 
 getBKData(formDateTime?: string, toDateTime?: string) {
     if (!formDateTime && !toDateTime) {
-      // http://52.207.62.240:8081/bkdata/getdata
-    return this.http.get('http://52.207.62.240:8081/bkdata/getdata');
+      // http://13.233.74.217:8081/bkdata/getdata
+    return this.http.get('http://13.235.240.6:8081/bkdata/getdata');
     } else {
       // tslint:disable-next-line:max-line-length
-      return this.http.get(`http://52.207.62.240:8081/bkdata/findByDateRange?from_datetime=${formDateTime}&to_datetime=${toDateTime}`);
+      return this.http.get(`http://13.235.240.6:8081/bkdata/findByDateRange?from_datetime=${formDateTime}&to_datetime=${toDateTime}`);
     }
   }
 
   addOrUpdateMobileNoConfig(data: any){
-    return this.http.post('http://52.207.62.240:8081/bkdata/mobile-plant-config/save',data,{responseType: 'text'});
+    return this.http.post('http://13.235.240.6:8081/bkdata/mobile-plant-config/save',data,{responseType: 'text'});
   }
 
   getMobileNumberConfig(){
-    return this.http.get('http://52.207.62.240:8081/bkdata/mobile-plant-config');
+    return this.http.get('http://13.235.240.6:8081/bkdata/mobile-plant-config');
   }
 
   addOrUpdateBakeTimeConfig(data: any){
-    return this.http.post('http://52.207.62.240:8081/bkdata/bkconfig/save',data,{responseType: 'text'});
+    return this.http.post('http://13.235.240.6:8081/bkdata/bkconfig/save',data,{responseType: 'text'});
   }
 
   getBakeTimeConfig(){
-    return this.http.get('http://52.207.62.240:8081/bkdata/bkconfig');
+    return this.http.get('http://13.233.74.217:8081/bkdata/bkconfig');
   }
 }
